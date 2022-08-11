6bd6dd717412e09522971196b748d6d89a671a2e
Arm Bun <bellotaophyc@outlook.com>
2022-08-11 18:15:19 +0000

    isolating my environments via workspaces

--- main.tf
+++ main.tf
@@ -1,14 +1,19 @@
-resource "aws_instance" "warmspace" {
-  ami = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
-}
-
-terraform {
-    backend "s3" {
-        bucket = "warmup-running-state"
-        key = "workspaces-warmspace/terraform.tfstate"
-        region = "us-east-2"
-        dynamodb_table = "warmup-running-locks"
-        encrypt = true
+    provider "aws" {
+            region = "us-east-2"
     }
-}
+
+
+    resource "aws_instance" "warmspace" {
+    ami = "ami-0c55b159cbfafe1f0"
+    instance_type = terraform.workspace == "default" ? "t2.medium" : "t2.micro"
+    }
+
+    terraform {
+        backend "s3" {
+            bucket = "warmup-running-state"
+            key = "workspaces-warmspace/terraform.tfstate"
+            region = "us-east-2"
+            dynamodb_table = "warmup-running-locks"
+            encrypt = true
+        }
+    }
